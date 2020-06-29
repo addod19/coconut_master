59df8f4813fd622631c87bd063664a60864b8cfb
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2020-06-29 11:22:56 +0000

    delete all files except the build directory

--- src/components/Nav/Style.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-import { MyNav } from 'carbon-components-react/lib/components/UIShell'
-
-const StyledMyNav = styled(props => MyNav)`
-  backgroundColor: 'green';
-  width: 100%;
-`
-
-export default StyledMyNav;
