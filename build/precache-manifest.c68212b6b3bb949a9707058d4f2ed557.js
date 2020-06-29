97816a444b47c14bec7ff97ddf60be7fa39c1c37
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2020-06-29 11:16:39 +0000

    gh-pages deployed

---
+++ build/precache-manifest.c68212b6b3bb949a9707058d4f2ed557.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ed0659e5648e26bf9ec7aa7662b0b31",
+    "url": "/coconut_master/index.html"
+  },
+  {
+    "revision": "3f3c21aa7aa93a23640c",
+    "url": "/coconut_master/static/css/main.f20c213f.chunk.css"
+  },
+  {
+    "revision": "fca711eac1c5a168c169",
+    "url": "/coconut_master/static/js/2.12cd1469.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/coconut_master/static/js/2.12cd1469.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f3c21aa7aa93a23640c",
+    "url": "/coconut_master/static/js/main.510169df.chunk.js"
+  },
+  {
+    "revision": "cca3175563a979f0ffe5",
+    "url": "/coconut_master/static/js/runtime-main.8aa6ed7d.js"
+  },
+  {
+    "revision": "87d18efa8cc8fed467158ac64a86050f",
+    "url": "/coconut_master/static/media/coco01.87d18efa.jpg"
+  },
+  {
+    "revision": "3caa69dad789d5c0602e8ce1900829ab",
+    "url": "/coconut_master/static/media/coco02.3caa69da.jpg"
+  },
+  {
+    "revision": "1914ff70a87a511c79db3e4c6ee67e0a",
+    "url": "/coconut_master/static/media/coco1.1914ff70.jpg"
+  },
+  {
+    "revision": "a658cc1fbb5bece534d842a53a146281",
+    "url": "/coconut_master/static/media/coco5.a658cc1f.jpg"
+  },
+  {
+    "revision": "bc51ef2958ca3b42d2269a4d139a0d2c",
+    "url": "/coconut_master/static/media/coco6.bc51ef29.jpg"
+  },
+  {
+    "revision": "9380ee53dedd917f531c683bec87aeb9",
+    "url": "/coconut_master/static/media/coco9.9380ee53.png"
+  },
+  {
+    "revision": "24709fc527181d7b1aa7e5a7229fcd67",
+    "url": "/coconut_master/static/media/coconut.24709fc5.svg"
+  },
+  {
+    "revision": "3203396c2145b655d3871be5eaf2e01c",
+    "url": "/coconut_master/static/media/img.3203396c.jpg"
+  },
+  {
+    "revision": "d44d29349c8989c16bd1717cef747e5c",
+    "url": "/coconut_master/static/media/img1.d44d2934.jpg"
+  },
+  {
+    "revision": "e9a490840f6c1f9317b0512e16512224",
+    "url": "/coconut_master/static/media/img3.e9a49084.jpg"
+  },
+  {
+    "revision": "71e5ece2b475238f4d2c8564b48fb7cb",
+    "url": "/coconut_master/static/media/img4.71e5ece2.jpg"
+  }
+]);
